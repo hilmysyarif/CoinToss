963b14cd93bf3630dc509159711b1fa27e2d9642
maqist <maqist@maqist.net>
2013-08-31 00:37:50 +0100

    0.3 Moved images and edited script to allow easier change in future.

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
+var odds = 0.50;
+var coin = "florin";
+var StartCoin =function(){
+	var startCoin=(coin)+"/heads.png";
+	document.images[0].src = (startCoin);};
 var CoinToss = function() {
     var side = Math.random();
-    if (side <0.50 ){
-    side= "tails.png";
-    } else if (side >0.50 ){
+    if (side >(odds) ){
+		side= "tails.png";
+    } else if (side <(odds) ){
         side= "heads.png";
     } else {
         side = "edge.png";}
-        return side;
+        return (coin)+"/"+side;
 }; 
 var TossCoin = function() {
-	setTimeout(function() {document.images[0].src = "coin.gif";},500);
+	var coinGif =(coin)+"/coin.gif";
+	setTimeout(function() {document.images[0].src = (coinGif);},250);
 	setTimeout(function() {document.images[0].src = CoinToss();},3000);
-//	document.images[0].src = CoinToss();
-	}
+	};
+var ChangeOdds = function () {
+	var chance = prompt("Please a number between 0-100, this is the percent chance of getting 'heads' ");
+	odds = chance / 100;
+	};
+//var ChangeCoin = function () {
+//	coin = prompt("Please select new coin");
+//		};
